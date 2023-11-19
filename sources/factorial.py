720aa77b3ba905c781cf48246db7e33848f4a70b
othmenbelgacem <othmanbelgacem8@gmail.com>
2023-11-19 11:34:20 +0100

    commit add calculate power

--- sources/factorial.py
+++ sources/factorial.py
@@ -4,20 +4,38 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
+def power(base, exponent):
+    return base ** exponent
+
 def main():
     try:
-        # Get user o input for the number
-        num = int(input("Enter a non-negative integer: "))
+        # Ask the user what operation they want to perform
+        operation = input("Choose an operation (factorial or power): ").lower()
 
-        # Check if the input test is non-negative
-        if num < 0:
-            print("Please enter a non-negative integer Thank you.")
+        if operation == "factorial":
+            # Get user input for the number
+            num = int(input("Enter a non-negative integer: "))
+
+            # Check if the input is non-negative
+            if num < 0:
+                print("Please enter a non-negative integer. Thank you.")
+            else:
+                result = factorial(num)
+                print("The factorial of {} is: {}".format(num, result))
+
+        elif operation == "power":
+            # Get user input for the base and exponent
+            base = int(input("Enter the base: "))
+            exponent = int(input("Enter the exponent: "))
+            
+            result = power(base, exponent)
+            print("{} raised to the power of {} is: {}".format(base, exponent, result))
+
         else:
-            result = factorial(num)
-            print("The factoral of {} is: {}".format(num, result))
+            print("Invalid operation. Please choose either 'factorial' or 'power'.")
 
     except ValueError:
-        print("Invalid input. Please enter a valid non-negative integer.")
+        print("Invalid input. Please enter valid integers.")
 
 if __name__ == "__main__":
     main()
